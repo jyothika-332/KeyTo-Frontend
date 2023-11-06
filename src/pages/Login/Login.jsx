dae04c782ad6dc1fe0bceec0910e925f82d4454a
jyothika-332 <jyothikact10@gmail.com>
2023-11-06 22:48:00 +0530

    changed

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import { LoginPage } from '../../components/Login/Login'
+import { LoginPage } from '../../components/login/Login'
 import { StickyNavbar } from '../../components/navbar/Navbar'
-
 function Login() {
   return (
     <div>
       <StickyNavbar/>
       <div className='mt-20'>
-        <LoginPage/>
+        <LoginPage/>  
       </div>
     </div>
   )
