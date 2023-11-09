e80f95646c18f8142e1386c65040168739300d5c
jyothika-332 <jyothikact10@gmail.com>
2023-11-09 19:25:27 +0530

    seller profile edit error fixed

--- src/components/dialogues/EditProfileSeller.jsx
+++ src/components/dialogues/EditProfileSeller.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Button,
   Dialog,
@@ -9,33 +9,39 @@ import {
 } from "@material-tailwind/react";
 import axios from "axios";
 import { BaseUrl } from "../../utils/Constants";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import { ShowToast } from "../../utils/Toats";
 
-
- 
-export function EditProfileSeller({data , setData , next }) {
-  const [open, setOpen] = React.useState(false); 
+export function EditProfileSeller({ data, setData, next }) {
+  const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(!open);
+  const [details, setDetails] = useState({
+    first_name: data.first_name,
+    last_name: data.last_name,
+    phone: data.phone,
+    address: data.address,
+  });
 
   const UpdateDatas = () => {
-    axios.patch(`${BaseUrl}/user/updateseller/${data.id}/`,data)
-    .then((res) => {
-      ShowToast("User Updated Succesfully", true)
-      next()
-      handleOpen()
-    })
-  }
+    axios
+      .patch(`${BaseUrl}/user/updateseller/${data.id}/`, details)
+      .then((res) => {
+        ShowToast("User Updated Suc}cesfully", true);
+        next();
+        handleOpen();
+      });
+  };
 
- 
   return (
     <>
-        <Button className="bg-deep-orange-500" onClick={handleOpen}>
-            Edit Profile
-        </Button>
+      <Button className="bg-deep-orange-500" onClick={handleOpen}>
+        Edit Profile
+      </Button>
       <Dialog open={open} handler={handleOpen}>
         <div className="flex items-center justify-between">
-          <DialogHeader className="text-deep-orange-900">Edit Profile</DialogHeader>
+          <DialogHeader className="text-deep-orange-900">
+            Edit Profile
+          </DialogHeader>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
@@ -52,31 +58,35 @@ export function EditProfileSeller({data , setData , next }) {
         </div>
         <DialogBody divider>
           <div className="grid gap-6">
-          <Input
-                label="First Name"
-                defaultValue={data.first_name ? data.first_name : ""}
-                onChange={(e) =>
-                  setData({ ...data, first_name: e.target.value })
-                }
-              />
-              <Input
-                label="Last Name"
-                defaultValue={data.last_name ? data.last_name : ""}
-                onChange={(e) =>
-                  setData({ ...data, last_name: e.target.value })
-                }
-              />
-              <Input
-                type="number"
-                label="Phone Number"
-                defaultValue={data.phone ? data.phone : ""}
-                onChange={(e) => setData({ ...data, phone: e.target.value })}
-              />
-              <Input
-                label="Address"
-                defaultValue={data.address ? data.address : ""}
-                onChange={(e) => setData({ ...data, address: e.target.value })}
-              />
+            <Input
+              label="First Name"
+              defaultValue={data.first_name ? data.first_name : ""}
+              onChange={(e) =>
+                setDetails({ ...details, first_name: e.target.value })
+              }
+            />
+            <Input
+              label="Last Name"
+              defaultValue={data.last_name ? data.last_name : ""}
+              onChange={(e) =>
+                setDetails({ ...details, last_name: e.target.value })
+              }
+            />
+            <Input
+              type="number"
+              label="Phone Number"
+              defaultValue={data.phone ? data.phone : ""}
+              onChange={(e) =>
+                setDetails({ ...details, phone: e.target.value })
+              }
+            />
+            <Input
+              label="Address"
+              defaultValue={data.address ? data.address : ""}
+              onChange={(e) =>
+                setDetails({ ...details, address: e.target.value })
+              }
+            />
           </div>
         </DialogBody>
         <DialogFooter className="space-x-2">
