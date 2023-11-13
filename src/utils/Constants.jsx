1b9b66a07d372a2e8d8c27f6d12ea5529b404a75
jyothika-332 <jyothikact10@gmail.com>
2023-11-13 13:42:37 +0530

    server url updations

--- src/utils/Constants.jsx
+++ src/utils/Constants.jsx
@@ -1,4 +1,4 @@
-export const BaseUrl = 'https://keyto.onrender.com';
+export const BaseUrl = 'https://sareespire.online';
 // export const BaseUrl = 'http://127.0.0.1:8000';
 
 export const WsURL = 'wss://keyto.onrender.com';
